a66faeef841939e5767e9c17580030de723f1b71
Camila <camila.rodriguezbrown@ucsm.edu.pe>
2024-02-27 14:56:44 -0500

    ñññ

--- service/servicefront/src/context/useService.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { TaskContext } from "./ServiceContext";
-
-export const useTasks = () => {
-  const context = useContext(TaskContext);
-  if (!context) throw new Error("useTasks must be used within a TaskProvider");
-  return context;
-};
