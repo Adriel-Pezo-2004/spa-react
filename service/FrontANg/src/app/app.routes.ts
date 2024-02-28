8ff7e1a55ebaedb78db88642373991c59aed5f09
Camila <camila.rodriguezbrown@ucsm.edu.pe>
2024-02-28 11:06:55 -0500

    ñññ

--- service/FrontANg/src/app/app.routes.ts
+++ service/FrontANg/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DateComponent } from './date/date.component';
+import { IssueComponent } from './issue/issue.component';
+import { ServiceComponent } from './service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'date', component: DateComponent },
+  { path: 'issue', component: IssueComponent },
+  { path: 'service', component: ServiceComponent },
+];
